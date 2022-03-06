899b332671670ff79cc0017e1ebf1b97b5297890
dressalsl <dressalsl97@gmail.com>
2022-03-06 11:53:42 -0300

    troca-grid

--- lib/main.dart
+++ lib/main.dart
@@ -29,13 +29,24 @@ class _RandomWordsState extends State<RandomWords> {
   final _saved = <String>{};
   final _biggerFont = const TextStyle(fontSize: 18);
   final _formKey = GlobalKey<FormState>();
+  bool isGrid = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
         actions: [
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+          IconButton(icon: Icon(Icons.star), onPressed: _pushSaved),
+          IconButton(
+              onPressed: () => setState(() {
+                    if (isGrid) {
+                      isGrid = false;
+                    } else {
+                      isGrid = true;
+                    }
+                  }),
+              icon: Icon(Icons.change_circle_outlined))
         ],
       ),
       body: _buildSuggestions(),
@@ -43,18 +54,35 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16),
-        itemBuilder: (BuildContext _context, int i) {
-          if (i.isOdd) {
-            return Divider();
-          }
-          final int index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.add(WordPair.random().asPascalCase);
-          }
-          return _buildRow(_suggestions[index], index);
-        });
+    switch (isGrid) {
+      case false:
+        return ListView.builder(
+            padding: const EdgeInsets.all(16),
+            itemBuilder: (BuildContext _context, int i) {
+              if (i.isOdd) {
+                return Divider();
+              }
+              final int index = i ~/ 2;
+              if (index >= _suggestions.length) {
+                _suggestions.add(WordPair.random().asPascalCase);
+              }
+              return _buildRow(_suggestions[index], index);
+            });
+      case true:
+        return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 1.5,
+            ),
+            itemBuilder: (BuildContext _context, int i) {
+              final int index = i;
+              if (index >= _suggestions.length) {
+                _suggestions.add(WordPair.random().asPascalCase);
+              }
+              return _buildRow(_suggestions[index], index);
+            });
+    }
+    return Text('');
   }
 
   Widget _buildRow(String name, int index) {
@@ -141,7 +169,7 @@ class _RandomWordsState extends State<RandomWords> {
     Navigator.of(context)
         .push(MaterialPageRoute<void>(builder: (BuildContext context) {
       return Scaffold(
-          appBar: AppBar(title: const Text('Go back')),
+          appBar: AppBar(title: const Text('Edit')),
           body: Wrap(
             children: <Widget>[
               Form(
